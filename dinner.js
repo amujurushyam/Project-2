a152177ea1e90ce9b9bcb85842c175f50d6a967f
amujurushyam <amujurushyam@gmail.com>
2025-02-13 23:51:13 +0530

    project-2

---
+++ dinner.js
@@ -0,0 +1,160 @@
+let openShopping = document.querySelector(".shopping");
+let closeShopping = document.querySelector(".closeShopping");
+let list = document.querySelector(".list");
+let listCard = document.querySelector(".listCard");
+let body = document.querySelector("body");
+let total = document.querySelector(".total");
+let quantity = document.querySelector(".quantity");
+
+openShopping.addEventListener("click", () => {
+  body.classList.add("active");
+});
+closeShopping.addEventListener("click", () => {
+  body.classList.remove("active");
+});
+
+let products = [
+  {
+    strMeal: "BeaverTails",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg",
+    idMeal: "52928",
+    price: 500,
+  },
+  {
+    strMeal: "Breakfast Potatoes",
+    strMealThumb: "https://www.themealdb.com/images/media/meals/1550441882.jpg",
+    idMeal: "52965",
+    price: 500,
+  },
+  {
+    strMeal: "Canadian Butter Tarts",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg",
+    idMeal: "52923",
+    price: 500,
+  },
+  {
+    strMeal: "Montreal Smoked Meat",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/uttupv1511815050.jpg",
+    idMeal: "52927",
+    price: 500,
+  },
+  {
+    strMeal: "Nanaimo Bars",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/vwuprt1511813703.jpg",
+    idMeal: "52924",
+    price: 500,
+  },
+  {
+    strMeal: "Pate Chinois",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/yyrrxr1511816289.jpg",
+    idMeal: "52930",
+    price: 500,
+  },
+  {
+    strMeal: "Pouding chomeur",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/yqqqwu1511816912.jpg",
+    idMeal: "52932",
+    price: 500,
+  },
+  {
+    strMeal: "Poutine",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg",
+    idMeal: "52804",
+    price: 500,
+  },
+  {
+    strMeal: "Rappie Pie",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/ruwpww1511817242.jpg",
+    idMeal: "52933",
+    price: 500,
+  },
+  {
+    strMeal: "Split Pea Soup",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/xxtsvx1511814083.jpg",
+    idMeal: "52925",
+    price: 500,
+  },
+  {
+    strMeal: "Sugar Pie",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/yrstur1511816601.jpg",
+    idMeal: "52931",
+    price: 500,
+  },
+  {
+    strMeal: "Timbits",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/txsupu1511815755.jpg",
+    idMeal: "52929",
+    price: 500,
+  },
+];
+let listCards = [];
+function initApp() {
+  products.forEach((value, key) => {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("item");
+    newDiv.innerHTML = `
+            <img src = "${value.strMealThumb}"/>
+      <div class="title">${value.strMeal}</div>
+      <div class="price">${value.price.toLocaleString()}</div>
+      <button onclick="addToCard(${key})">Add To Card</button>
+      `;
+    list.appendChild(newDiv);
+  });
+}
+initApp();
+function addToCard(key) {
+  if (listCards[key] == null) {
+    // copy product form list to list card
+    listCards[key] = JSON.parse(JSON.stringify(products[key]));
+    listCards[key].quantity = 1;
+  }
+  reloadCard();
+}
+function reloadCard() {
+  listCard.innerHTML = "";
+  let count = 0;
+  let totalPrice = 0;
+  listCards.forEach((value, key) => {
+    totalPrice = totalPrice + value.price;
+    count = count + value.quantity;
+    if (value != null) {
+      let newDiv = document.createElement("li");
+      newDiv.innerHTML = `
+                <div><img src="${value.strMealThumb}"/></div>
+                <div>${value.strMeal}</div>
+                <div>${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${
+        value.quantity - 1
+      })">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${
+        value.quantity + 1
+      })">+</button>
+                </div>`;
+      listCard.appendChild(newDiv);
+    }
+  });
+  total.innerText = totalPrice.toLocaleString();
+  quantity.innerText = count;
+}
+function changeQuantity(key, quantity) {
+  if (quantity == 0) {
+    delete listCards[key];
+  } else {
+    listCards[key].quantity = quantity;
+    listCards[key].price = quantity * products[key].price;
+  }
+  reloadCard();
+}
---
+++ seafood.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const recipeList = document.getElementById("recipe-list");
+  let currentIndex = 0;
+
+  fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+    .then((response) => response.json())
+    .then((data) => {
+      data.meals.forEach((meal) => {
+        const recipe = document.createElement("div");
+        recipe.classList.add("recipe");
+
+        const recipeImg = document.createElement("img");
+        recipeImg.src = meal.strMealThumb;
+        recipeImg.alt = meal.strMeal;
+
+        const recipeName = document.createElement("div");
+        recipeName.classList.add("name");
+        recipeName.textContent = meal.strMeal;
+
+        const addToCart = document.createElement("button");
+        addToCart.classList.add("add-to-cart3");
+        addToCart.innerHTML = "Add To Cart";
+
+        recipe.appendChild(recipeImg);
+        recipe.appendChild(recipeName);
+        recipe.appendChild(addToCart);
+        recipeList.appendChild(recipe);
+      });
+
+      const totalRecipes = data.meals.length;
+
+      setInterval(() => {
+        currentIndex = (currentIndex + 1) % totalRecipes;
+        recipeList.style.transform = `translateX(-${
+          currentIndex * (100 / totalRecipes)
+        }%)`;
+      }, 3000);
+    })
+    .catch((error) => console.error("Error fetching the recipes:", error));
+});
